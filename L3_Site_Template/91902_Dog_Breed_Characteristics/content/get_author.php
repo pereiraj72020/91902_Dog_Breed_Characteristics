379078f2303336262b11315ba0000e3d24f1c273
pereiraj72020 <51728484+pereiraj72020@users.noreply.github.com>
2021-08-24 10:10:26 +1200

    added alt first and alt last names

--- L3_Site_Template/91902_Dog_Breed_Characteristics/content/get_author.php
+++ L3_Site_Template/91902_Dog_Breed_Characteristics/content/get_author.php
@@ -4,7 +4,9 @@
     $first = $find_rs['First'];
     $middle = $find_rs['Middle'];
     $last = $find_rs['Last'];
+    $altfirst = $find_rs['AltFirst'];
+    $altlast = $find_rs['AltLast'];
     
-    $full_name = $first." ".$middle." ".$last;
+    $full_name = $first." ".$middle." ".$last." ".$altfirst." ".$altlast;
 
     ?>
