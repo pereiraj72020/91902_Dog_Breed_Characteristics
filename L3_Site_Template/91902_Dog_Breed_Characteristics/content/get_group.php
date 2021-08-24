78f217c16df5469d2590bddf5d41242fe04ae120
pereiraj72020 <51728484+pereiraj72020@users.noreply.github.com>
2021-08-25 11:36:37 +1200

    changing variables to suit database in content folder.

--- L3_Site_Template/91902_Dog_Breed_Characteristics/content/get_group.php
+++ L3_Site_Template/91902_Dog_Breed_Characteristics/content/get_group.php
@@ -1,3 +1,3 @@
-    $sub_sql = "SELECT * FROM `subject` WHERE `Subject_ID` = $subject";
+    $sub_sql = "SELECT * FROM `group` WHERE `Group_ID` = $group";
     $sub_query = mysqli_query($dbconnect, $sub_sql);
     $sub_rs = mysqli_fetch_assoc($sub_query);
